a219d2c783ba6d294af012b5ca35a8845e2d5e38
OKran0706 <93201241+OKran0706@users.noreply.github.com>
2023-05-03 13:29:51 +0530

    Update Server_Thread.py

--- Test Environment/Method_1_Timestamp_LSB/Server_Thread.py
+++ Test Environment/Method_1_Timestamp_LSB/Server_Thread.py
@@ -15,6 +15,16 @@ def handle_packet1(packet):
 		print("Incoming data len:", n)
 
 
+startf=[0]
+def handle_3way(packet):
+    if packet[TCP].flags=="S":
+            syn_ack_pkt = IP(dst=packet[IP].src)/TCP(dport=packet[TCP].sport, sport=packet[TCP].dport, flags="SA", chksum=0,seq=0, ack=packet[TCP].seq + 1)
+            send(syn_ack_pkt)
+    elif packet[TCP].flags=="A":
+          startf[0]=1
+          
+sniff(filter='(tcp dst port 1234)', prn=handle_3way,stop_filter=lambda p: startf[0])
+            
 
 
 
@@ -23,6 +33,7 @@ bits=[]
 
 sniff(filter='icmp', prn=handle_packet1,count=1)
 print("Waiting for",n[0],"packets")
+
 
 
     
@@ -37,9 +48,25 @@ def handle_packet(packet):
                 print("TCP Timestamp:", timestamp_value)
                 bits.append(str(timestamp_value[0]%2))
                 print("Bits Received",len(bits))
+                reply_pkt = IP(dst=packet[IP].src)/TCP(dport=packet[TCP].sport, sport=packet[TCP].dport, flags="A", chksum=0,seq=packet[TCP].ack, ack=packet[TCP].seq + 1)
+                send(reply_pkt)
 
 # Start sniffing for incoming packets
 sniff(filter='(tcp dst port 1234)', prn=handle_packet,count=n[0])
+
+
+
+startf=[0]
+def handle_3wayFin(packet):
+    if packet[TCP].flags=="FA":
+            syn_ack_pkt = IP(dst=packet[IP].src)/TCP(dport=packet[TCP].sport, sport=packet[TCP].dport, flags="A", chksum=0,seq=packet[TCP].ack, ack=packet[TCP].seq + 1)
+            send(syn_ack_pkt)
+            syn_ack_pkt = IP(dst=packet[IP].src)/TCP(dport=packet[TCP].sport, sport=packet[TCP].dport, flags="RA", chksum=0,seq=packet[TCP].ack, ack=packet[TCP].seq + 1)
+            send(syn_ack_pkt)
+            startf[0]=1
+          
+sniff(filter='(tcp dst port 1234)', prn=handle_3wayFin,stop_filter=lambda p: startf[0])
+
 
 #Create Bitstring
 bitstring="".join(bits)
@@ -55,5 +82,6 @@ bytes_data = num.to_bytes(num_bytes, byteorder='big')
 #Write to file
 f=open("msg.txt","w")
 f.write(bytes_data.decode('utf-8'))
+
 
 
