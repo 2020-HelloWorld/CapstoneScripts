21787e5a40ca07e221d4050497bfb7874daacdce
Ghanashyam Bhat <74894094+Ghanashyam-Bhat@users.noreply.github.com>
2023-10-17 12:07:45 +0530

    DT

---
+++ Layer2/classifier.py
@@ -0,0 +1,39 @@
+import json
+
+def descisionTree(u,nr_packets,c,multimod):
+    if u == 1:
+        return False
+    else:
+        if 25<c<50:
+            return False
+        elif c<25:
+            if nr_packets<170:
+                return False
+            else:
+                return True 
+        else:
+            if u/c > 0.9:
+                return False
+            else:
+                if multimod > 0.7:
+                    return False
+                else:
+                    return True
+
+with open("flow_analysis.json") as jsonFIle:
+    flowData = json.load(jsonFIle)
+
+for flow in flowData:
+    src = flow["flow_id"][0]
+    dst = flow["flow_id"][2]
+    c = flow["c"]
+    multimod = flow["multimod"]
+    u = flow["unique"]
+    nr_mode = flow["nrPacketsMode"]
+    nr_packets = flow["nrPackets"]
+    multimod2 = flow["multimod2"]
+    widthwave = flow["widthave"]
+    autocorr = flow["autocorr"]
+
+    print(src,"\t-\t",dst,"\t\t",descisionTree(u,nr_packets,c,multimod))
+
